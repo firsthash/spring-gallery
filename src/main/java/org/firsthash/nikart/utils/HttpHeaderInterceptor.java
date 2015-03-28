3e4be5e39fcbd70caba3ed4b3cfba9bb0ea9b010
Yuriy Liskov <firsthash@gmail.com>
2015-03-28 02:52:59 +0200

    bugfix

--- src/main/java/org/firsthash/nikart/utils/HttpHeaderInterceptor.java
+++ src/main/java/org/firsthash/nikart/utils/HttpHeaderInterceptor.java
@@ -14,7 +14,8 @@ public class HttpHeaderInterceptor extends HandlerInterceptorAdapter {
     @Override
     public void postHandle(HttpServletRequest request,
                 HttpServletResponse response,
-                Object handler)
+                Object handler,
+                ModelAndView modelAndView)
          throws Exception {
         response.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
         response.setHeader("Pragma", "no-cache");
