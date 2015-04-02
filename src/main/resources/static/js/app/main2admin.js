d3260e42f9e383d17bee47dafaada2c25ad77caa
Yuriy Liskov <firsthash@gmail.com>
2015-04-03 01:21:05 +0300

    admin panel

--- src/main/resources/static/js/app/main2admin.js
+++ src/main/resources/static/js/app/main2admin.js
@@ -31,7 +31,7 @@ define(['app/AppView2Admin', 'data'], function(AppView2Admin, data) {
             return this.cid;
         },
         isRoot: false,
-        hideFields: "content-title content-description position url",
+        hideFields: "content-title content-description url style",
         render: function(){
             this.$el.append(this.template());
             this.collection.each(function(model){
