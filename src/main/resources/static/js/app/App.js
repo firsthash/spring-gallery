839e87e0ccc6fe65a638552be8a1ac1bab384c6f
Yuriy Liskov <firsthash@gmail.com>
2015-09-01 02:56:40 +0300

    remove old code

---
+++ src/main/resources/static/js/app/App.js
@@ -0,0 +1,27 @@
+define(['data', 'backbone', 'app/view/MenuViewDecorator'], function(data, Backbone, MenuViewDecorator) {
+
+    // top-level piece of UI
+    return Backbone.View.extend({
+        // Instead of generating a new element, bind to the existing element
+        el: $("body"),
+
+        // bind collection events, when items are added or changed
+        initialize: function(){
+            console.assert(data, 'initial data is not defined');
+
+            var view = new MenuViewDecorator({data: data});
+            // for debugging purposes
+            window.menuview = view;
+        },
+
+        // add one item
+        addOne: function(model) {
+        },
+
+        // batch add item
+        addAll: function() {
+            this.collection.each(this.addOne, this);
+        }
+
+    });
+});
